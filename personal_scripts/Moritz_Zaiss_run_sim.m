13b2dd59bcc20a1509436852d65dbc2a077c5176
mzaiss <moritz.zaiss@uk-erlangen.de>
2022-09-15 14:07:09 +0200

    added my scripts

---
+++ personal_scripts/Moritz_Zaiss_run_sim.m
@@ -0,0 +1,51 @@
+lib_path='D:\root\LABLOG\FAU\MRIlab\SIM\pulseq-cest-library';
+seq_path=[lib_path '/seq-library/'];
+sim_path=[lib_path '/sim-library/'];
+
+seq_filename=fullfile(seq_path,'/APTw_3T_003_2uT_8block_DC95_834ms_braintumor/APTw_3T_003_2uT_8block_DC95_834ms_braintumor.seq')
+
+
+seq_filename=fullfile(seq_path,'/APTw_3T_000_2uT_1block_2s_braintumor/APTw_3T_000_2uT_1block_2s_braintumor.seq')
+seq_filename=fullfile(lib_path,'/sandbox/001_realisticAPTw/WASABI_3T_001_3p7uT_1block_5ms.seq')
+
+% read the .seq-file
+
+file_list= {
+    'WM_3T_default_7pool_bmsim.yaml'
+    'z_phantom_creatine_3T_pH6.4_T22C_bmsim.yaml'
+    };
+
+%% 1
+figure('Name','APTw_000 WM');
+seq_filename=fullfile(seq_path,'/APTw_3T_000_2uT_1block_2s_braintumor/APTw_3T_000_2uT_1block_2s_braintumor.seq')
+seq = mr.Sequence;  seq.read(seq_filename);
+offsets_ppm = seq.definitions('offsets_ppm'); % offsets
+m0_offset = seq.definitions('M0_offset');     % m0 offset frequency
+M_z = simulate_pulseqcest(seq_filename,[sim_path 'WM_3T_default_7pool_bmsim.yaml']);
+plotSimulationResults(M_z,offsets_ppm,m0_offset);
+
+legend(shortID, 'Interpreter', 'none');
+
+%% 2
+figure('Name','APTw_000 Cr');
+seq_filename=fullfile(seq_path,'/APTw_3T_000_2uT_1block_2s_braintumor/APTw_3T_000_2uT_1block_2s_braintumor.seq')
+seq = mr.Sequence;  seq.read(seq_filename);
+offsets_ppm = seq.definitions('offsets_ppm'); % offsets
+m0_offset = seq.definitions('M0_offset');     % m0 offset frequency
+M_z = simulate_pulseqcest(seq_filename,[sim_path 'z_phantom_creatine_3T_pH6.4_T22C_bmsim.yaml']);
+plotSimulationResults(M_z,offsets_ppm,m0_offset);
+
+legend(shortID, 'Interpreter', 'none');
+
+%% 3
+figure('Name','WASABI WM');
+seq_filename=fullfile(lib_path,'/sandbox/002_BMsim_challenge/WASABI_3T_001_3p7uT_1block_5ms.seq')
+seq = mr.Sequence;  seq.read(seq_filename);
+offsets_ppm = seq.definitions('offsets_ppm'); % offsets
+m0_offset = seq.definitions('M0_offset');     % m0 offset frequency
+M_z = simulate_pulseqcest(seq_filename,[sim_path 'WM_3T_default_7pool_bmsim.yaml']);
+plotSimulationResults(M_z,offsets_ppm,m0_offset);
+
+legend(shortID, 'Interpreter', 'none');
+
+
