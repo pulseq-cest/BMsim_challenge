dc6e80731af59abb2e7cb2d32dda5d26370770c5
mzaiss <moritz.zaiss@uk-erlangen.de>
2022-09-15 14:14:29 +0200

    fixed APTw

--- seq/APTw_3T_000_2uT_1block_2s_braintumor/APTw_3T_000_2uT_1block_2s_braintumor.m
+++ seq/APTw_3T_000_2uT_1block_2s_braintumor/APTw_3T_000_2uT_1block_2s_braintumor.m
@@ -1,11 +1,13 @@
-%% APTw_3T_003_2uT_8block_DC95_834ms_braintumor.seq
-% Creates a sequence file for an APTw protocol with block pulses, ~95% DC and t_sat of 833 ms:
+%% APTw_3T_000_2uT_1block_2s_braintumor
+% Creates a sequence file for an APTw protocol with on single cw (block) pulse
+% this sequence will not run on some real Systems.
+% it serves as a reference for the pulsed pre-saturation schemes
 %
-% Kai Herz 2020
+% Moritz Zaiss 2021
 % kai.herz@tuebingen.mpg.de
 
 % author name for sequence file
-author = 'Kai Herz';
+author = 'Moritz Zaiss';
 
 %% get id of generation file
 if contains(mfilename, 'LiveEditorEvaluationHelperESectionEval')
@@ -16,17 +18,17 @@ end
 
 %% sequence definitions
 % everything in seq_defs gets written as definition in .seq-file
-seq_defs.n_pulses      = 8               ; % number of pulses
-seq_defs.tp            = 100e-3          ; % pulse duration [s]
-seq_defs.td            = [1e-3 10e-3]    ; % interpulse delay [s]
+seq_defs.n_pulses      = 1              ; % number of pulses
+seq_defs.tp            = 2           ; % pulse duration [s]
+seq_defs.td            = 0            ; % interpulse delay [s]
 seq_defs.Trec          = 3.5             ; % recovery time [s]
 seq_defs.Trec_M0       = 3.5             ; % recovery time before M0 [s]
 seq_defs.M0_offset     = -1560           ; % m0 offset [ppm]
-seq_defs.DCsat         = (2*seq_defs.tp)/(2*seq_defs.tp+sum(seq_defs.td)); % duty cycle
-seq_defs.offsets_ppm   = [seq_defs.M0_offset -4:0.25:4]; % offset vector [ppm]
+seq_defs.DCsat         = (seq_defs.tp)/(seq_defs.tp+seq_defs.td); % duty cycle
+seq_defs.offsets_ppm   = [seq_defs.M0_offset -15:0.25:15]; % offset vector [ppm]
 seq_defs.num_meas      = numel(seq_defs.offsets_ppm)   ; % number of repetition
-seq_defs.Tsat          = seq_defs.n_pulses/2*(seq_defs.tp+seq_defs.td(1)) + ...
-                         seq_defs.n_pulses/2*(seq_defs.tp+seq_defs.td(2)) - seq_defs.td(2);  % saturation time [s]
+seq_defs.Tsat          = seq_defs.n_pulses*(seq_defs.tp+seq_defs.td) - ...
+                         seq_defs.td ;  % saturation time [s]
 seq_defs.B0            = 3               ; % B0 [T]
 seq_defs.seq_id_string = seqid           ; % unique seq id
 
@@ -54,9 +56,12 @@ gyroRatio_hz  = 42.5764;                  % for H [Hz/uT]
 gyroRatio_rad = gyroRatio_hz*2*pi;        % [rad/uT]
 fa_sat        = B1pa*gyroRatio_rad*tp; % flip angle of sat pulse
 % create pulseq saturation pulse object
-satPulse      = mr.makeBlockPulse(fa_sat, 'Duration', tp, 'system', seq.sys);
 
-[B1cwpe,B1cwae,B1cwae_pure,alpha]= calculatePowerEquivalents(satPulse,tp,sum(td)/2,1,gyroRatio_hz);
+%satPulse      = mr.makeGaussPulse(fa_sat, 'Duration', t_p,'system',lims,'timeBwProduct', 0.2,'apodization', 0.5); % siemens-like gauss
+%satPulse      = mr.makeSincPulse(fa_sat, 'Duration', tp, 'system', lims,'timeBwProduct', 2,'apodization', 0.15); % philips-like sinc
+satPulse      = mr.makeBlockPulse(fa_sat, 'Duration', tp, 'system', seq.sys); % block pusle cw
+
+[B1cwpe,B1cwae,B1cwae_pure,alpha]= calculatePowerEquivalents(satPulse,tp,td,1,gyroRatio_hz);
 seq_defs.B1cwpe = B1cwpe;
 
 
@@ -81,20 +86,16 @@ for currentOffset = offsets_Hz
         seq.addBlock(satPulse) % add sat pulse
         % calc phase for next rf pulse
         accumPhase = mod(accumPhase + currentOffset*2*pi*(numel(find(abs(satPulse.signal)>0))*1e-6),2*pi);
-        
         if np < n_pulses % delay between pulses
-            if mod(np,2) == 0
-                seq.addBlock(mr.makeDelay(td(2)));
-            else
-                seq.addBlock(mr.makeDelay(td(1))); % add delay
-            end
+            seq.addBlock(mr.makeDelay(td)); % add delay
         end
     end
     if spoiling % spoiling before readout
-        seq.addSpoilerGradients();
+        seq.addSpoilerGradients()
     end
     seq.addPseudoADCBlock(); % readout trigger event
 end
+
 
 
 %% write definitions
@@ -104,16 +105,11 @@ for n_id = 1:numel(def_fields)
 end
 seq.write(seq_filename, author);
 
-%% write sequence
-seq.write(seq_filename);
-
 %% plot
 saveSaturationPhasePlot(seq_filename);
-
+warndlg(' This sequence will not run on all scanners with DC limits and can crash the scanner software! Use pulsed sequences instead' )
 %% call standard sim
 M_z = simulate_pulseqcest(seq_filename,'../../sim-library/WM_3T_default_7pool_bmsim.yaml');
-
-%% plot
-plotSimulationResults(M_z,offsets_ppm, seq_defs.M0_offset);
-
+figure('Name','Z-asym');
+plotSimulationResults(M_z,offsets_ppm,seq_defs.M0_offset);
 
---
+++ seq/APTw_3T_000_2uT_1block_2s_braintumor/Readme.md
@@ -0,0 +1,14 @@
+## APTw_3T_000_2uT_1block_2s_braintumor
+
+### Description
+APTw brain tumor protocol for 3T
+* B<sub>1,cwpe</sub> = 2 µT
+* T<sub>sat</sub> =2 s
+* DC<sub>sat</sub> = 100 %
+
+### Publication
+
+
+### Plot
+<img src="APTw_3T_000_2uT_1block_2s_braintumor.png" width="300"/> 
+
